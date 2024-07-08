182d122fd357c57666fa32320994cd9eb6a783f6
Oleg Shevelev <oshevelyov@gmail.com>
2024-07-08 14:40:42 +0100

    Modified calls to Pocket API to fix stats

--- src/colifer/reportextenders/articles/pocket_parser.py
+++ src/colifer/reportextenders/articles/pocket_parser.py
@@ -1,6 +1,6 @@
 import datetime
 
-import pocket
+from colifer.reportextenders.articles import pocket
 from colifer.config import Config
 from colifer.namingrules import NamingRules
 from colifer.reportextenders.report_extender import ReportExtender
@@ -15,11 +15,15 @@ class PocketParser(ReportExtender):
     def __init__(self, section_entries):
         super().__init__(section_entries)
         if Config.get_section_param(section_entries, "enabled"):
-            self.pocket_consumer_key = Config.get_section_param(section_entries, "consumer_key")
-            self.pocket_access_token = Config.get_section_param(section_entries, "access_token")
-            self.api = pocket.Pocket(self.pocket_consumer_key, self.pocket_access_token)
+            self.pocket_consumer_key = Config.get_section_param(
+                section_entries, "consumer_key")
+            self.pocket_access_token = Config.get_section_param(
+                section_entries, "access_token")
+            self.api = pocket.Pocket(
+                self.pocket_consumer_key, self.pocket_access_token)
             self.naming_rules = NamingRules()
-            naming_rules_filename = Config.get_section_param(section_entries, "naming_rules_file")
+            naming_rules_filename = Config.get_section_param(
+                section_entries, "naming_rules_file")
             self.naming_rules.read_naming_rules(naming_rules_filename)
         else:
             self.api = None
@@ -41,23 +45,29 @@ class PocketParser(ReportExtender):
                 exit(1)
 
             init_section_path_elements = path.split(SECTION_SEPARATOR)
-            report.find_or_create_section(report.root_section, init_section_path_elements, False)
+            report.find_or_create_section(
+                report.root_section, init_section_path_elements, False)
 
             result = self.api.get(state='archive', detailType='simple')
             if result is not None and len(result) > 0 and result[0]['list'] is not None:
                 for key in result[0]['list'].keys():
                     section_path_elements = init_section_path_elements.copy()
-                    time_read = datetime.datetime.fromtimestamp(int(result[0]['list'][key]['time_read']))
+                    time_read = datetime.datetime.fromtimestamp(
+                        int(result[0]['list'][key]['time_read']))
                     if report_parameters.period_start <= time_read <= report_parameters.period_end:
                         init_section_path_elements = section_path_elements.copy()
                         if "quora.com" in self.safe_get(result[0]['list'][key], 'resolved_url', 'given_url'):
                             section_path_elements.append("Quora")
                         print("Processing Pocket article: " + self.safe_get(result[0]['list'][key],
                                                                             'resolved_title', 'given_title'))
-                        word_count = self.safe_get(result[0]['list'][key], 'word_count', None)
-                        article_title = self.safe_get(result[0]['list'][key], 'resolved_title', 'given_title')
-                        article_url = self.safe_get(result[0]['list'][key], 'resolved_url', 'given_url')
+                        word_count = self.safe_get(
+                            result[0]['list'][key], 'word_count', None)
+                        article_title = self.safe_get(
+                            result[0]['list'][key], 'resolved_title', 'given_title')
+                        article_url = self.safe_get(
+                            result[0]['list'][key], 'resolved_url', 'given_url')
                         # TODO: Find a way to make links agnostic to generated format (atm it's md)
                         section_path_elements.append("[{}]({}) ({} words)".format(article_title, article_url,
                                                                                   word_count))
-                        report.find_or_create_section(report.root_section, section_path_elements, False)
+                        report.find_or_create_section(
+                            report.root_section, section_path_elements, False)
--- src/colifer/reportextenders/articles/pocket_stats.py
+++ src/colifer/reportextenders/articles/pocket_stats.py
@@ -1,7 +1,7 @@
 """Module to gather basic Pocket stats."""
 
 import logging
-
+from time import sleep
 from colifer.config import Config
 from colifer.constants import LOGGING_FORMAT
 from colifer.reportextenders.articles.pocket_parser import PocketParser
@@ -9,15 +9,34 @@ from colifer.reportextenders.articles.pocket_parser import PocketParser
 logging.basicConfig(level="INFO", format=LOGGING_FORMAT)
 logger = logging.getLogger(__name__)
 
+MAX_RETURN_ITEMS = 300
+
 
 def main():
     config = Config()
-    pocker_parser = PocketParser(config.get_param('Pocket'))
-    result_unread = pocker_parser.api.get(state='unread', detailType='simple')
-    result_archived = pocker_parser.api.get(state='archived', detailType='simple')
-    logger.info(f"\nPocket stats: \n"
-                f"   Unread queue size: {len(result_unread[0]['list'])}\n"
-                f"   Number archived: {len(result_archived[0]['list'])}")
+    pocket_parser = PocketParser(config.get_param('Pocket'))
+    result_unread = pocket_parser.api.get(state='unread', detailType='simple')
+    is_finished = False
+    offset = 0
+    total_archived_count = 0
+    while not is_finished:
+        result_archived = pocket_parser.api.get(
+            state='archive', detailType='simple', offset=offset, count=MAX_RETURN_ITEMS)
+        if not result_archived or 'list' not in result_archived[0]:
+            logger.error("Incorrect response from API: %s", result_archived)
+            break
+        archived_count = len(result_archived[0]['list'])
+        if archived_count < MAX_RETURN_ITEMS:
+            is_finished = True
+        total_archived_count += archived_count
+        logger.info("Requesting archived: %s...", total_archived_count)
+        offset += MAX_RETURN_ITEMS
+    print(result_archived)
+    logger.info("\nPocket stats: \n"
+                "   Unread queue size: %s\n"
+                "   Number archived: %s", len(result_unread[0]['list']), total_archived_count)
+
+
 # time_added
 if __name__ == '__main__':
     main()
